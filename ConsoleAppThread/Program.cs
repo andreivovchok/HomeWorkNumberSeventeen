903f3bc3aed10223eefaa39eb2b23a09b0d7da96
Andrei Vovchok <andreivovchok@bk.ru>
2021-09-17 19:59:03 +0300

    Добавил решение задания

---
+++ ConsoleAppThread/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using HomeWorkNumberSeventeen;
+
+namespace ConsoleAppThread
+{
+    class Program
+    {
+        static void Action(List<Car> cars)
+        {          
+            foreach (var car in cars)
+            {
+                car.Age = (343 * 34 * 2 * 77) / 567;
+            }
+        }
+
+        static void Action1(List<Car> cars)
+        {
+            for (int i = 0; i < cars.Count; i++)
+            {
+                cars[i].Age = (343 * 34 * 2 * 77) / 567;
+            }
+        }
+
+        static void Action2(List<Car> cars)
+        {
+            Parallel.ForEach(cars, car => { car.Age = (343 * 34 * 2 * 77) / 567; });
+        }
+
+        static void Action3(List<Car> cars)
+        {
+            Parallel.For(0, cars.Count, index => { cars[index].Age = (343 * 34 * 2 * 77) / 567; });
+        }
+
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < 100000000; i++)
+            {
+                cars.Add(new Car(i));
+            }
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            Action(cars);
+            Console.WriteLine(stopwatch.Elapsed);
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            Action1(cars);
+            Console.WriteLine(stopwatch.Elapsed);
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            Action2(cars);
+            Console.WriteLine(stopwatch.Elapsed);
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            Action3(cars);
+            Console.WriteLine(stopwatch.Elapsed);
+            stopwatch.Reset();
+        }
+    }
+}
---
+++ HomeWorkNumberSeventeen/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HomeWorkNumberSeventeen
+{
+    public class Car
+    {
+        private int _age;
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public Car(int age)
+        {
+            _age = age;
+        }
+    }
+}
